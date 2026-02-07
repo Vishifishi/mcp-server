5564b70833b624291965b119c8e00d589dcf863b
Vishifishi <ritulroshan.ravindran@gmail.com>
2026-02-07 11:25:30 -0800

    added calories

--- bmi/views.py
+++ bmi/views.py
@@ -13,16 +13,24 @@ def home(request):
 
             if bmi < 18.5:
                 category = "Underweight"
+                calorie_adjustment = 300
             elif bmi < 25:
                 category = "Normal"
+                calorie_adjustment = 0
             elif bmi < 30:
                 category = "Overweight"
+                calorie_adjustment = -300
             else:
                 category = "Obese"
+                calorie_adjustment = -500
+
+            base_calories = weight * 30
+            recommended_calories = int(base_calories + calorie_adjustment)
 
             context = {
                 "bmi": bmi,
-                "category": category
+                "category": category,
+                "calorie_adjustment": recommended_calories
             }
 
         except Exception:
